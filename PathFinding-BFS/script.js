dafe161988510e9cf43127170edd0cc759368560
Julian Nathaniel Cagadas <juliannathanielcagadas@gmail.com>
2026-07-23 11:31:04 +0800

    Add grid bfs playground (Pending)

--- PathFinding-BFS/script.js
+++ PathFinding-BFS/script.js
@@ -1,1 +1,10 @@
+const grid = document.querySelector(".grid");
+const cell = document.createElement("div");
 
+for (let i = 0; i < 100; i++) {
+  const cell = document.createElement("div");
+  cell.className = "cell";
+  grid.appendChild(cell);
+}
+console.log(grid);
+console.log(cell);
